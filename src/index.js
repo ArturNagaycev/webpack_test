8474f0801b61ed6e3f8c0ffcbe7f81b4ddf0fc15
Artur Nagaycev <artur.nagaycev@gmail.com>
2023-02-10 16:54:45 +0200

    installed lodash, added uniq.js with imported lodash function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const sum = require('./sum');
+const { numbers, uniqueNumbers } = require('./uniq');
 
-const result = sum(1, 2);
+const a = 5;
+const b = 5;
+const result = sum(a, b);
 
-console.log(result);
+console.log(`Result of sum function: ${a} + ${b} = ${result}`);
+console.log(`Unique numbers from array [${numbers}] are ${uniqueNumbers}`);
---
+++ src/uniq.js
@@ -0,0 +1,6 @@
+const _ = require('lodash');
+
+const numbers = [2, 1, 2, 7, 7];
+const uniqueNumbers = _.uniq([...numbers]);
+
+module.exports = { numbers, uniqueNumbers };
